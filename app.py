777506c0ab25662ef328f11a6a71c0b7158ca198
PacocaApps <gpaaadm@gmail.com>
2020-05-10 19:42:37 -0300

    AAAAAAAA

--- app.py
+++ app.py
@@ -3,56 +3,56 @@ from flask import Flask
 import socket         
 
 app = Flask(__name__)
+PORT1 = int(os.environ.get('PORT', 5000))
 
-
-
-
-print("first part app running")
-
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    print("if1 running")
-    port = int(os.environ.get('PORT', 5000))
-
-
-    print("second app part running")
-    s = socket.socket()          
+print("second app part running")
+s = socket.socket()          
     
-    s.bind(('', port))   
-    print ("Socket successfully created")
-    # first of all import the socket library 
+s.bind(('', PORT1))   
+print ("Socket successfully created")
+# first of all import the socket library 
 
-    s.listen(5)      
-    print ("socket is listening")           
+s.listen(5)      
+print ("socket is listening")           
   
-    # a forever loop until we interrupt it or  
-    #  an error occurs 
-    while True: 
+# a forever loop until we interrupt it or  
+#  an error occurs 
+while True: 
   
-    # Establish connection with client. 
-      c, addr = s.accept()      
-      print ('Got connection from', addr)
+# Establish connection with client. 
+ c, addr = s.accept()      
+ print ('Got connection from', addr)
   
    # send a thank you message to the client.  
-      c.send('Thank you for connecting') 
+ c.send('Thank you for connecting') 
   
    # Close the connection with the client 
-      c.close() 
+ c.close() 
 
-
-
-    app.run(host='0.0.0.0', port=port)
-    print("if running")
-
-
-
-
-
+print("first part app running")
 
 @app.route('/')
 def hello():
     print("hello running")
     return 'Sever Running!'
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    print("if1 running")
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    print("if running")
+
+    
+
+
+
+  
+
+
+
+
+
 
 
     
