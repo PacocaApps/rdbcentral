071d80603922c66b74b9214eddb2d3b8ae0b8f9a
PacocaApps <gpaaadm@gmail.com>
2020-05-10 18:49:25 -0300

    socket comms1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+import socket         
 
 app = Flask(__name__)
 
@@ -11,3 +12,44 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    
+    
+s.bind(('', port))         
+print ("socket binded to %s", %(port) )
+  
+
+
+s = socket.socket()          
+print ("Socket successfully created")
+# first of all import the socket library 
+       
+  
+# next create a socket object 
+
+  
+# reserve a port on your computer in our 
+# case it is 12345 but it can be anything 
+  
+# Next bind to the port 
+# we have not typed any ip in the ip field 
+# instead we have inputted an empty string 
+# this makes the server listen to requests  
+# coming from other computers on the network 
+
+# put the socket into listening mode 
+s.listen(5)      
+print ("socket is listening")           
+  
+# a forever loop until we interrupt it or  
+# an error occurs 
+while True: 
+  
+   # Establish connection with client. 
+   c, addr = s.accept()      
+   print 'Got connection from', addr 
+  
+   # send a thank you message to the client.  
+   c.send('Thank you for connecting') 
+  
+   # Close the connection with the client 
+   c.close() 
