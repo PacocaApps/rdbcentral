2f6171f2282da737a1e6865012a82390f9e59f00
PacocaApps <gpaaadm@gmail.com>
2020-05-09 21:42:46 -0300

    0.1

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import http.server
-import socketserver
+import os
+from flask import Flask
+app = Flask(__name__)
 
-PORT = 17995
-Handler = http.server.SimpleHTTPRequestHandler
+@app.route("/")
+def hello():
+    return "Hello from Python!"
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
